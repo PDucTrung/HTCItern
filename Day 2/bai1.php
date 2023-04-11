fd3fd7dc060c06414af8a739afb80f7224f56569
trung <trungezeal115@gmail.com>
2023-04-11 17:55:58 +0700

    update bai 2

--- Day 2/bai1.php
+++ Day 2/bai1.php
@@ -5,33 +5,33 @@ $breakPoint = 15;
 function splitString($str, $bp)
 {
     $start = 0;
-    $numString = 1;
+    $Child = 1;
     while ($start < strlen($str) - 1) {
         if ($start + $bp >= strlen($str)) {
-            echo ("Child " . $numString . ":");
+            echo ("Child " . $Child . ":");
             for ($i = $start; $i < strlen($str); $i++) {
                 echo ($str[$i]);
             }
             echo "\n";
             $start = strlen($str) - 1;
         } else if ($str[$start + $bp] == " ") {
-            echo ("Child " . $numString . ":");
+            echo ("Child " . $Child . ":");
             for ($i = $start; $i < $start + $bp; $i++) {
                 echo ($str[$i]);
             }
             echo "\n";
             $start += $bp + 1;
-            $numString++;
+            $Child++;
         } else if ($str[$start + $bp] != " ") {
             for ($j = $start + $bp; $j < strlen($str); $j++) {
                 if ($str[$j] == " " || $j == strlen($str) - 1) {
-                    echo ("Child " . $numString . ":");
+                    echo ("Child " . $Child . ":");
                     for ($i = $start; $i < $j; $i++) {
                         echo ($str[$i]);
                     }
                     echo "\n";
                     $start = $j + 1;
-                    $numString++;
+                    $Child++;
                     break;
                 }
             }
