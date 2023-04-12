5a2151454943e9bec12e8485473505a0fb2310f6
trung <trungezeal115@gmail.com>
2023-04-12 10:30:28 +0700

    ui bai 2

--- Day 2/bai1.php
+++ Day 2/bai1.php
@@ -1,42 +1,70 @@
 <?php
-$string = "Chung toi toi HTC hoc Magento";
+$string = "Chung toi den HTC hoc Magento Chung toi den HTC hoc Magento";
 $breakPoint = 15;
 
-function splitString($str, $bp)
+function splitString(string $string, int $breakPoint)
 {
-    $start = 0;
-    $Child = 1;
-    while ($start < strlen($str) - 1) {
-        if ($start + $bp >= strlen($str)) {
-            echo ("Child " . $Child . ":");
-            for ($i = $start; $i < strlen($str); $i++) {
-                echo ($str[$i]);
-            }
-            echo "\n";
-            $start = strlen($str) - 1;
-        } else if ($str[$start + $bp] == " ") {
-            echo ("Child " . $Child . ":");
-            for ($i = $start; $i < $start + $bp; $i++) {
-                echo ($str[$i]);
-            }
-            echo "\n";
-            $start += $bp + 1;
-            $Child++;
-        } else if ($str[$start + $bp] != " ") {
-            for ($j = $start + $bp; $j < strlen($str); $j++) {
-                if ($str[$j] == " " || $j == strlen($str) - 1) {
-                    echo ("Child " . $Child . ":");
-                    for ($i = $start; $i < $j; $i++) {
-                        echo ($str[$i]);
-                    }
-                    echo "\n";
-                    $start = $j + 1;
-                    $Child++;
-                    break;
+    $count = 0;
+    if ($string[$breakPoint - 1] != " ") {
+        for ($i = $breakPoint; $i < strlen($string); $i++) {
+            if ($string[$i] == " ") {
+                for ($j = 0; $j < $i; $j++) {
+                    $count++;
+                    echo $string[$j];
                 }
+                break;
             }
         }
+    } else {
+        for ($i = 0; $i < $breakPoint; $i++) {
+            $count++;
+            echo $string[$i];
+        }
+    }
+
+    echo "\n";
+    for ($i = $count; $i < strlen($string); $i++) {
+        echo $string[$i];
     }
 }
 
 echo splitString($string, $breakPoint);
+
+// function splitString($str, $bp)
+// {
+//     $start = 0;
+//     $Child = 1;
+//     while ($start < strlen($str) - 1) {
+//         if ($start + $bp >= strlen($str)) {
+//             echo ("Child" . $Child . " = ");
+//             for ($i = $start; $i < strlen($str); $i++) {
+//                 echo ($str[$i]);
+//             }
+//             echo "\n";
+//             $start = strlen($str) - 1;
+//         } else if ($str[$start + $bp] == " ") {
+//             echo ("Child" . $Child . " = ");
+//             for ($i = $start; $i < $start + $bp; $i++) {
+//                 echo ($str[$i]);
+//             }
+//             echo "\n";
+//             $start += $bp + 1;
+//             $Child++;
+//         } else if ($str[$start + $bp] != " ") {
+//             for ($j = $start + $bp; $j < strlen($str); $j++) {
+//                 if ($str[$j] == " " || $j == strlen($str) - 1) {
+//                     echo ("Child" . $Child . " = ");
+//                     for ($i = $start; $i < $j; $i++) {
+//                         echo ($str[$i]);
+//                     }
+//                     echo "\n";
+//                     $start = $j + 1;
+//                     $Child++;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// echo splitString($string, $breakPoint);
