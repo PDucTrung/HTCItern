c6ba51e60051d50047a4e97eda990676319b77fc
trung <trungezeal115@gmail.com>
2023-04-28 09:05:36 +0700

    search time

--- oop/quanly.php
+++ oop/quanly.php
@@ -10,8 +10,10 @@ $db = new database();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý nhân sự</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/style.css" />
 </head>
 
@@ -24,27 +26,32 @@ $db = new database();
                 <!-- tabs -->
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="dev-tab" data-bs-toggle="tab" data-bs-target="#dev" type="button" role="tab" aria-controls="dev" aria-selected="true">
+                        <button class="nav-link active" id="dev-tab" data-bs-toggle="tab" data-bs-target="#dev"
+                            type="button" role="tab" aria-controls="dev" aria-selected="true">
                             Developer
                         </button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="manager-tab" data-bs-toggle="tab" data-bs-target="#manager" type="button" role="tab" aria-controls="manager" aria-selected="false">
+                        <button class="nav-link" id="manager-tab" data-bs-toggle="tab" data-bs-target="#manager"
+                            type="button" role="tab" aria-controls="manager" aria-selected="false">
                             Manager
                         </button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="work-tab" data-bs-toggle="tab" data-bs-target="#work" type="button" role="tab" aria-controls="work" aria-selected="false">
+                        <button class="nav-link" id="work-tab" data-bs-toggle="tab" data-bs-target="#work" type="button"
+                            role="tab" aria-controls="work" aria-selected="false">
                             Công việc
                         </button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="salary-tab" data-bs-toggle="tab" data-bs-target="#salary" type="button" role="tab" aria-controls="salary" aria-selected="false">
+                        <button class="nav-link" id="salary-tab" data-bs-toggle="tab" data-bs-target="#salary"
+                            type="button" role="tab" aria-controls="salary" aria-selected="false">
                             hệ số lương
                         </button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="thongke-tab" data-bs-toggle="tab" data-bs-target="#thongke" type="button" role="tab" aria-controls="thongke" aria-selected="false">
+                        <button class="nav-link" id="thongke-tab" data-bs-toggle="tab" data-bs-target="#thongke"
+                            type="button" role="tab" aria-controls="thongke" aria-selected="false">
                             Thống kê
                         </button>
                     </li>
@@ -80,7 +87,8 @@ $db = new database();
     </section>
 
     <!-- script -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 
 </body>
@@ -88,17 +96,17 @@ $db = new database();
 </html>
 
 <script>
-    // delete get id
+// delete get id
 
-    function del(id, action) {
-        let deleteId = document.querySelector('#' + action);
-        deleteId.value = id;
-    };
+function del(id, action) {
+    let deleteId = document.querySelector('#' + action);
+    deleteId.value = id;
+};
 
-    // edit get id
+// edit get id
 
-    function edit(id, action) {
-        let editId = document.querySelector('#' + action);
-        editId.value = id;
-    };
+function edit(id, action) {
+    let editId = document.querySelector('#' + action);
+    editId.value = id;
+};
 </script>
--- oop/view/thongke/index.php
+++ oop/view/thongke/index.php
@@ -2,11 +2,15 @@
     <div class="sreach-box">
         <form action="" method="get">
             <div class="d-flex gap-3 align-items-center">
-                <div><strong>Lọc theo Số giờ</strong></div>
-                <input type="number" value="<?php echo (isset($_GET["valuestart"]) && $_GET["valuestart"] != "" ? $_GET["valuestart"] : "null") ?>" class="form-control ip-search" name="valuestart">
+                <div><strong>Lọc theo số giờ</strong></div>
+                <input type="number" placeholder="number" min=0
+                    value="<?php echo (isset($_GET["valuestart"]) && $_GET["valuestart"] != "" ? $_GET["valuestart"] : "null") ?>"
+                    class="form-control ip-search" name="valuestart" require>
                 <div>To</div>
-                <input type="number" value="<?php echo (isset($_GET["valueend"]) && $_GET["valueend"] != "" ? $_GET["valueend"] : "null") ?>" class="form-control ip-search" name="valueend">
-                <button class="btn btn-primary" type="submit">Search</button>
+                <input type="number" placeholder="number"
+                    value="<?php echo (isset($_GET["valueend"]) && $_GET["valueend"] != "" ? $_GET["valueend"] : "null") ?>"
+                    class="form-control ip-search" name="valueend" require>
+                <button class="btn btn-primary" min=0 type="submit">Search</button>
             </div>
         </form>
     </div>
@@ -21,58 +25,61 @@
         $asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
         $add_class = ' class="highlight"';
     ?>
-        <table>
-            <thead>
-                <tr>
-                    <th>
-                        <a href="quanly.php?column=ten&order=<?php echo $asc_or_desc; ?>&pagethongke=<?php echo $current_page; ?>&valuestart=<?php echo $valuestart; ?>&valueend=<?php echo $valueend; ?>">
-                            Tên nhân viên
-                            <i class="fas fa-sort"></i>
-                        </a>
-                    </th>
-                    <th>
-                        <a href="quanly.php?column=luong&order=<?php echo $asc_or_desc; ?>&pagethongke=<?php echo $current_page; ?>&valuestart=<?php echo $valuestart; ?>&valueend=<?php echo $valueend; ?>">Lương
-                            1 tháng
-                            <i class="fas fa-sort"></i>
-                        </a>
-                    </th>
-                    <th>
-                        <a href="quanly.php?column=sogio&order=<?php echo $asc_or_desc; ?>&pagethongke=<?php echo $current_page; ?>&valuestart=<?php echo $valuestart; ?>&valueend=<?php echo $valueend; ?>">
-                            Sốgiờ
-                            <i class="fas fa-sort"></i>
-                        </a>
-                    </th>
-                </tr>
-            </thead>
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    <a
+                        href="quanly.php?column=ten&order=<?php echo $asc_or_desc; ?>&pagethongke=<?php echo $current_page; ?>&valuestart=<?php echo $valuestart; ?>&valueend=<?php echo $valueend; ?>">
+                        Tên nhân viên
+                        <i class="fas fa-sort"></i>
+                    </a>
+                </th>
+                <th>
+                    <a
+                        href="quanly.php?column=luong&order=<?php echo $asc_or_desc; ?>&pagethongke=<?php echo $current_page; ?>&valuestart=<?php echo $valuestart; ?>&valueend=<?php echo $valueend; ?>">Lương
+                        1 tháng
+                        <i class="fas fa-sort"></i>
+                    </a>
+                </th>
+                <th>
+                    <a
+                        href="quanly.php?column=sogio&order=<?php echo $asc_or_desc; ?>&pagethongke=<?php echo $current_page; ?>&valuestart=<?php echo $valuestart; ?>&valueend=<?php echo $valueend; ?>">
+                        Số giờ
+                        <i class="fas fa-sort"></i>
+                    </a>
+                </th>
+            </tr>
+        </thead>
 
-            <tbody>
-                <?php while ($row = mysqli_fetch_assoc($thongke)) { ?>
-                    <tr>
-                        <td <?php echo $column == 'ten' ? $add_class : ''; ?>>
-                            <?php echo $row["ten"] ?>
-                        </td>
-                        <td <?php echo $column == 'luong' ? $add_class : ''; ?>>
-                            <?php echo $row["luong"] ?>
-                        </td>
-                        <td <?php echo $column == 'sogio' ? $add_class : ''; ?>>
-                            <?php echo $row["sogio"] ?>
-                        </td>
-                    </tr>
+        <tbody>
+            <?php while ($row = mysqli_fetch_assoc($thongke)) { ?>
+            <tr>
+                <td <?php echo $column == 'ten' ? $add_class : ''; ?>>
+                    <?php echo $row["ten"] ?>
+                </td>
+                <td <?php echo $column == 'luong' ? $add_class : ''; ?>>
+                    <?php echo $row["luong"] ?>
+                </td>
+                <td <?php echo $column == 'sogio' ? $add_class : ''; ?>>
+                    <?php echo $row["sogio"] ?>
+                </td>
+            </tr>
 
-                <?php } ?>
-                <?php mysqli_free_result($thongke) ?>
-                <tr>
-                    <td colspan='3'>
-                        <nav aria-label='Page navigation panigation'>
-                            <ul class='pagination'>
-                                <?php
+            <?php } ?>
+            <?php mysqli_free_result($thongke) ?>
+            <tr>
+                <td colspan='3'>
+                    <nav aria-label='Page navigation panigation'>
+                        <ul class='pagination'>
+                            <?php
                                 include "pagination.php"
                                 ?>
-                            </ul>
-                        </nav>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+                        </ul>
+                    </nav>
+                </td>
+            </tr>
+        </tbody>
+    </table>
     <?php } ?>
 </div>
