646339d00ba9d923c133f12c0f914b8f673eb32a
trung <trungezeal115@gmail.com>
2023-05-04 16:16:08 +0700

    new db

--- oop/database/qlns.sql
+++ oop/database/qlns.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 04, 2023 lúc 03:51 AM
+-- Thời gian đã tạo: Th5 04, 2023 lúc 11:15 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.1.17
 
@@ -71,7 +71,7 @@ INSERT INTO `manager` (`StaffID`, `level`) VALUES
 
 CREATE TABLE `soefficientsalary` (
   `StaffID` int(11) NOT NULL,
-  `hesoluong` int(11) NOT NULL
+  `hesoluong` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -79,12 +79,12 @@ CREATE TABLE `soefficientsalary` (
 --
 
 INSERT INTO `soefficientsalary` (`StaffID`, `hesoluong`) VALUES
-(1, 2),
-(2, 1),
-(4, 3),
-(5, 2),
-(3, 3),
-(6, 1),
+(1, 3),
+(2, 1.5),
+(4, 3.5),
+(5, 1.5),
+(3, 2.5),
+(6, 1.5),
 (7, 3);
 
 -- --------------------------------------------------------
@@ -110,7 +110,7 @@ CREATE TABLE `staff` (
 INSERT INTO `staff` (`StaffID`, `ten`, `tuoi`, `diachi`, `ngaysinh`, `namkinhnghiem`, `luongcoban`) VALUES
 (4, 'Tiền', 23, 'Bắc Ninh', '2000-03-03', 2, 3000000),
 (5, 'Thuần', 23, 'Nam sách', '2000-02-02', 1, 1000000),
-(1, 'Trung', 23, 'Hải Dương', '2000-04-04', 3, 2),
+(1, 'Trung', 23, 'Hải Dương', '2000-04-04', 2, 3000000),
 (3, 'Tuyền', 21, 'Nam Định', '2000-11-11', 1, 500000),
 (6, 'trung6', 23, 'Nam sách', '2000-02-02', 2, 1000000),
 (7, 'Long', 23, 'hai duong', '2000-04-04', 1, 4000000),
@@ -141,7 +141,7 @@ INSERT INTO `work` (`StaffID`, `sogio`, `thang`, `nam`) VALUES
 (5, 120, 12, 2),
 (3, 250, 14, 2),
 (6, 250, 1, 2),
-(7, 100, 3, 2);
+(7, 120, 5, 3);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
