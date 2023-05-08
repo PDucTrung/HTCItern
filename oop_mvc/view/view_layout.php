48212f8e1f5c7a26fb2d28ea5061c71d1b376ee4
trung <trungezeal115@gmail.com>
2023-05-08 15:39:10 +0700

    fix view

--- oop_mvc/view/view_layout.php
+++ oop_mvc/view/view_layout.php
@@ -5,49 +5,50 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QLNS</title>
+    <title>HRM</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="public/assets/style.css" />
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="index.php">HTC</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.php?controller=worker">Nhân Viên</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.php?controller=choose_work">Thời gian làm việc</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.php?controller=choose_statis">Thống kê</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">HTC</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav m-auto w-50">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?controller=worker">Nhân Viên</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?controller=choose_work">Thời gian làm việc</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?controller=choose_statis">Thống kê</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-12 mt-5">
-                <?php
-                if (file_exists($controller)) {
-                    include_once $controller;
-                } else {
-                    echo "<div class='alert alert-success mt-5 text-center w-75 m-auto'><h4>Welcome to HRM!</h4></div>";
-                }
-                ?>
-            </div>
-        </div>
-    </div>
+        </nav>
+    </header>
 
+    <section class="section_view_table mt-5">
+        <div class="container">
+            <?php
+            if (file_exists($controller)) {
+                include_once $controller;
+            } else {
+                echo "<div class='alert alert-success mt-5 text-center w-75 m-auto'><h4>Welcome to HRM!</h4></div>";
+            }
+            ?>
+        </div>
+    </section>
+
+    <footer class="mt-5">
+    </footer>
     <!-- script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
--- oop_mvc/view/work/view_choose_work.php
+++ oop_mvc/view/work/view_choose_work.php
@@ -1,12 +1,12 @@
-<div class="p-3 border border-secondary m-auto col-6">
+<div class="p-3 border border-secondary m-auto col-4">
     <div class="panel-body">
         <form method="post" action="<?php echo $form_action ?>" class="d-flex flex-column gap-4 justify-content-center">
 
             <h5 class="text-center">Thống kê thời gian làm theo nhân viên</h5>
 
-            <div class="row d-flex align-items-center">
-                <div class="col-md-5">Chọn nhân viên muốn xem</div>
-                <div class="col-md-7">
+            <div class="row d-flex justify-content-center gap-3">
+                <p class="col-md-8 text-center">Chọn nhân viên muốn xem</p>
+                <div class="col-md-6">
                     <select name="name_worker" class="form-select">
                         <?php $arr_name = $this->model->fetch("select * from tbl_worker");
                         foreach ($arr_name as $name) {
