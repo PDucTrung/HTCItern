959b7dc85318442b73de00cb7b3860b94f386317
trung <trungezeal115@gmail.com>
2023-06-05 17:54:46 +0700

    fist

---
+++ SuggestProducts/Block/Popup.php
@@ -0,0 +1,9 @@
+<?php
+namespace Trung\SuggestProducts\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Popup extends Template
+{
+    protected $_template = 'Trung_SuggestProducts::popup.phtml';
+}
