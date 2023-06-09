400cc037a5722f5bcc256e916e096c245118da5d
trung <trungezeal115@gmail.com>
2023-06-09 11:09:35 +0700

    override proceed to checkout in mini cart

---
+++ SuggestProducts/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    'config': {
+        'mixins': {
+            'Magento_Checkout/js/view/minicart': {
+                'Trung_SuggestProducts/js/checkout/view/minicart': true
+            },
+        }
+    }
+};
---
+++ SuggestProducts/view/frontend/web/js/checkout/view/minicart.js
@@ -0,0 +1,54 @@
+define([
+    'jquery',
+    'Magento_Customer/js/model/authentication-popup',
+    'Magento_Ui/js/modal/modal',
+    'Magento_Customer/js/customer-data'
+], function ($, authenticationPopup, modal, customerData
+) {
+    'use strict';
+
+    return function (Component) {
+        return Component.extend({
+
+            /**
+             * @override
+             */
+            getCartParam: function (name) {
+                var self = this;
+
+                const showPopup = (event) => {
+                    event.preventDefault();
+                    var customer = customerData.get('customer');
+                    if (customer().firstname) {
+                        var options = {
+                            type: 'popup',
+                            responsive: true,
+                            innerScroll: true,
+                            modalClass: 'custom-modal-class',
+                            buttons: [{
+                                text: $.mage.__('Close'),
+                                class: 'mymodal-close',
+                                click: function() {
+                                    this.closeModal();
+                        }
+                    }]
+                };
+
+                if(name === 'possible_onepage_checkout'){                           
+                    var popup = modal(options, $('#customModal'));
+                    $("#customModal").modal("openModal");
+                        return false;
+                    }
+                };
+
+                $('#top-cart-btn-checkout').on('click',showPopup);
+                    
+                }
+
+                $('.checkout').on('click',showPopup);
+                
+                return this._super(name);
+            },
+        });
+    }
+});
